45cd524d170536113de9778af3b17767d8476f46
Gross <L1GROSS@bridgew.edu>
2023-02-21 12:19:11 -0500

    Initial commit

---
+++ src/main/java/bsu/comp152/Main.java
@@ -0,0 +1,8 @@
+package bsu.comp152;
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rect = new Rectangle();
+        System.out.println(rect);
+    }
+}
---
+++ src/main/java/bsu/comp152/Rectangle.java
@@ -0,0 +1,12 @@
+package bsu.comp152;
+
+public class Rectangle {
+
+    // Declare instance variables.
+    // Instance variables are in contrast to static variables.
+    private double length;
+    private double width;
+    // These capture the data associated with Rectangle objects.
+
+
+}
