078b23f48a97ed3faef47f1737cca5535bbc6d6a
lkgross <profgross@gmail.com>
2023-02-23 12:20:02 -0500

    Add more detail

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -1,8 +1,42 @@
 package bsu.comp152;
 
+/**
+ * Is a (container) class for the Java program we run. It contains a main method with client code (a driver program) to create and manipulate Rectangle objects.
+ */
 public class Main {
     public static void main(String[] args) {
-        Rectangle rect = new Rectangle();
+        /*
+         * Create a Rectangle object.
+         * Call it rect.
+         * Do an inline initialization of a Rectangle object.
+         * We instantiate the Rectangle class.
+         * We are creating an instance of the Rectangle class.
+         *
+         * There is a default constructor that takes 0 arguments.
+         * However, it is no longer available after we write our own constructor in the Rectangle class.
+         */
+        // Rectangle rect = new Rectangle();
+
+        Rectangle rect = new Rectangle(5.4, 3.0);
         System.out.println(rect);
+        System.out.println(rect.getLength());
+        System.out.println(rect.getWidth());
+        rect.setLength(12.2);
+        System.out.println(rect.getLength());
+        rect.setWidth(9.17);
+        System.out.println(rect.getWidth());
+        /*
+         * Calling toString explicitly is not necessary.
+         */
+        System.out.println(rect.toString());
+        /*
+         * There is an implicit call to toString.
+         */
+        System.out.println(rect);
+        /*
+         * The most useful toString method will be called: the one we wrote in the Rectangle class, rather than the memory address.
+         */
+        Rectangle rect2 = new Rectangle(12.2, 9.17);
+        System.out.println(rect.equals(rect2));
     }
 }
--- src/main/java/bsu/comp152/Rectangle.java
+++ src/main/java/bsu/comp152/Rectangle.java
@@ -1,12 +1,69 @@
 package bsu.comp152;
 
+import org.w3c.dom.css.Rect;
+
+/**
+ * Is a (blueprint) class for creating and manipulating Rectangle objects.
+ */
 public class Rectangle {
 
-    // Declare instance variables.
+    // Declare instance variables at the top of the class.
     // Instance variables are in contrast to static variables.
     private double length;
     private double width;
     // These capture the data associated with Rectangle objects.
+
+    // Define instance methods.
+    /*
+     * Accessors (getters) return information about the rectangle.
+     * Mutators (setters) set (modify) information associated with the rectangles.
+     */
+
+    /*
+     * A constructor has the same name as the class.
+     * A constructor is public.
+     * A constructor has no return type, not even void!
+     */
+    public Rectangle(double len, double wid){
+        length = len;
+        width = wid;
+    }
+
+    /**
+     * A getter (accessor)
+     * @return the length of the rectangle
+     */
+    public double getLength(){
+        return length;
+    }
+
+
+    public double getWidth(){
+        return width;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Rectangle: length %f, width %f", length, width);
+    }
+
+    public boolean equals(Rectangle other){
+        return (length == other.length) && (width == other.width);
+    }
+
+    /**
+     * A setter (mutator)
+     * @param len the value to use for the length
+     */
+    public void setLength(double len){
+        length = len;
+    }
+
+    public void setWidth(double wid){
+        width = wid;
+    }
+
+
 
 
 }
